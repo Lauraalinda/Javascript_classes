f33908fe6c085b9995f5874545df7a4759034f64
Lauraalinda <lauraallindaa@gmail.com>
2022-07-25 06:39:54 +0300

    changes

--- assignment.js
+++ assignment.js
@@ -2,7 +2,7 @@ class Mkulima{
     constructor(){
         this.farm=[];
         this.groceryproducts=[];
-        this.vendors=[]
+        this.orders=[]
     
 this.addFarm=(farmId,farmname,farmer,phone,address)=>{
   this.farm.push({
@@ -54,51 +54,76 @@ this.getProduct=(productId)=>{
   let getProducts=this.groceryproducts.find(item=>item.productId===productId);
   return getProducts
  }
+this.printProduct=()=>{
+  return this.groceryproducts
+}
+this.calculateOrderCost=(productId,quantity)=>{
+let cost=this.groceryproducts.find(item=>item.productId===productId)
+console.log(`Total cost of your order is ${cost.pricefields*quantity}`)
+this.orders.push(cost)
+}
 }
 }
 
 let mkulima1=new Mkulima();
+//ADDING FARM
 mkulima1.addFarm(3465,"NapierValley","john","0756046099","p.oBox28244");
 console.log(mkulima1)
+
+//REMOVING FARM
 mkulima1.addFarm(1115,"NapierValley","john","0756046099","p.oBox28244");
 console.log(mkulima1.farm)
 mkulima1.removeFarm(1115);
 console.log(mkulima1.farm)
+
+//UPDATING FARM
 mkulima1.addFarm(0015,"keyValley","elaxi","07724344325","p.oBox01244");
 console.log(mkulima1.farm)
 mkulima1.updateFarm(0015,6678,"Kentonfarm","deogracious","075432113","p.obox4322")
 console.log(mkulima1.farm)
+
+//GETTING FARM
 console.log(mkulima1.getFarm(3465))
 
 
 
 let product1=new Mkulima();
-product1.addProduct(9994,"Cabbages","2000ugx");
+//ADDING PRODUCT
+product1.addProduct(9994,"Cabbages",2000);
 console.log(product1)
-product1.addProduct(5555,"Bananas","1000ugx");
+
+//REMOVING PRODUCT
+product1.addProduct(5555,"Bananas",1000);
 console.log(product1.groceryproducts)
 product1.removeProduct(5555);
 console.log(product1.groceryproducts)
-product1.addProduct()
-product1.addProduct(1222,"onions","300ugx");
+ 
+
+
+//UPDATING PRODUCT
+product1.addProduct(1222,"onions",300);
 console.log(product1.groceryproducts)
-product1.updateProduct(1222,3456,"ginger","9000ugx")
+product1.updateProduct(1222,3456,"ginger",9000)
 console.log(product1.groceryproducts)
+
+//GETTING PRODUCT
 console.log(product1.getProduct(9994))
 
+//PRINTING PRODUCTS
+product1.addProduct(1002,"beans",1000)
+product1.addProduct(1777,"maize","700ugx")
+product1.printProduct
+console.log(product1)
+
+
+//CALCULATE ORDER COST
+console.log(product1.calculateOrderCost(1002,4))
 
 
 
 
 
 
-
-
-// let mkulima2=new Mkulima();
-// mkulima2.addFarm(823889,"StoneValley","Siara","0772434325","p.oBox2345");
-// console.log(mkulima2.farm)
-// 
-// 
 
 
 
